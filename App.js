0d42059794a4ff642c8dc39511624ca8e7d5d241
whitehatjrdemo1 <70006536+whitehatjrdemo1@users.noreply.github.com>
2021-02-24 15:11:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Clock from "./components/clock"
+
+export default class App extends Component {
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+      
+        <Clock/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 40,
+    textAlign:"center",
+  }
+});
